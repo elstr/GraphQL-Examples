1fd9d454b9968df90ea61a01c7c7be6c62e22796
elstr <eleonora.lester@gmail.com>
2019-07-09 20:13:04 -0300

    feat: Adds createVideo fn

--- src/data/index.js
+++ src/data/index.js
@@ -25,7 +25,15 @@ const videos = [
   }
 ];
 
+const getNewId = title => new Buffer.from(title).toString("base64");
+
 exports.getVideoById = id =>
   new Promise(resolve => resolve(videos.find(v => v.id === id)));
 
-exports.getVideos = () => new Promise(resolve => resolve(videos))
+exports.getVideos = () => new Promise(resolve => resolve(videos));
+
+exports.createVideo = args => {
+  const video = {id: getNewId(args.title), ...args}
+  videos.push(video) // we mutate the array just to be able to see all videos in query videos
+  return video // this video will be returned by the resolve fn in the mutation
+}
