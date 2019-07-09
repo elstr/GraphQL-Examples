978046ebf274f8c3ccfa8b252ff7fdec0d8e8382
elstr <eleonora.lester@gmail.com>
2019-07-09 13:40:49 -0300

    initial-examples

---
+++ initial/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const { graphql, buildSchema } = require("graphql");
+
+// Describes what is possibly, and perhaps more importantly what's not possible
+// We query our schema for foo and it's a string
+const schema = buildSchema(`
+    type Video {
+        id: ID,
+        title: String,
+        duration: Int,
+        watched: Boolean
+    }
+    type Query {
+        video: Video
+    }
+    type Schema {
+        query: Query
+    }
+`);
+
+const video = {
+    id: "abc",
+    title: 'Create a GraphQL Schema',
+    duration: 120,
+    watched: false
+}
+
+// This is how we resolve the query for video, we return a video object
+const resolvers = {
+  video: () => video
+};
+
+const query = `{
+    video {
+      id,
+      title,
+      duration,
+      watched
+    }
+  }`;
+
+graphql(schema, query, resolvers)
+  .then(result => console.log(result))
+  .catch(err => console.log(err));
